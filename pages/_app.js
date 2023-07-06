10aa1c1d182c14f137134e47292844500324b314
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-07-06 22:44:36 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
+
   return <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=G-N447S94TC5`}
-                strategy="afterInteractive"
-              />
-              <Script id="google-analytics" strategy="afterInteractive">
-                {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-N447S94TC5');
-          `}
-          <Script/>
-          <Component {...pageProps} />
-        </>
-    
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=G-N447S94TC5`}
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-N447S94TC5');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
