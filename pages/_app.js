263f68df432fb2c6826cdb04c91d4aee3ee11dee
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-07-06 22:28:48 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=G-N447S94TC5`}
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N447S94TC5');
+          `}
+          />
+          <Component {...pageProps} />
+        </>
+    
 }
