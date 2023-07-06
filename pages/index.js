334c9cd79da820ccd5fa5079b769f70da0f50d61
sagarjaid <sagarjaid321@gmail.com>
2023-07-07 00:08:21 +0530

    UI improved

--- pages/index.js
+++ pages/index.js
@@ -36,23 +36,23 @@ export default function Home() {
 
         <meta name="google-site-verification" content="OZcQv8cI2bp_N6Uq0pBaX5LoGZ44JJtwQZo1iVCLPig" />
       </Head>
-      <main className="flex flex-col justify-center items-center h-screen gap-6 p-6">
+      <main className="flex flex-col justify-center items-center h-screen gap-5 p-6">
         <div>
           <img className="w-[120px]" src="./sagar.png" />
         </div>
         <div className="flex flex-col justify-center items-center text-center gap-1 ">
           <h1 className="text-xl font-extrabold">Sagar Jaid</h1>
-          <h2>A Software Engineer, Growth Hacker & Saas Founder</h2>
+          <h2>Software Engineer & Indie Hacker</h2>
           <div className="flex justify-center gap-4 mt-1 items-center">
             <a href="https://www.linkedin.com/in/sagarjaid/" target="_blank">
-              <img className="w-5 h-5" src="./linkedin.png" />
+              <img className="w-4 h-4" src="./linkedin.png" />
             </a>
             <a
               href="https://twitter.com/jaid_sagar"
               target="_blank"
               className="pt-1"
             >
-              <img className="w-[21px] h-[21px]" src="./twitter.png" />
+              <img className="w-[18px] h-[18px]" src="./twitter.png" />
             </a>
           </div>
         </div>
@@ -60,10 +60,10 @@ export default function Home() {
           <a
             href="http://saasblog.ai/"
             target="_blank"
-            className=" flex gap-2 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
+            className=" flex gap-1.5 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
           >
             <img className="w-16 h-16" src="./saasblog-logo.png" />
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col ">
               <h3 className="font-bold">SaaSblog</h3>
               <h4 className="text-[8px]">
                 Simple & Modern WordPress Blog Alternative for your SaaS
@@ -75,10 +75,10 @@ export default function Home() {
           <a
             href="http://chnglog.com/"
             target="_blank"
-            className="flex gap-2 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
+            className="flex gap-1.5 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
           >
             <img className="w-16 h-16" src="./chnglog-logo.png" />
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col ">
               <h3 className="font-bold">Chnglog</h3>
               <h4 className="text-[8px]">
                 Simplify Changelog by Effortlessly Manage your product updates
@@ -90,10 +90,10 @@ export default function Home() {
           <a
             href="http://scripai.com/"
             target="_blank"
-            className="flex gap-2 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
+            className="flex gap-1.5 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
           >
             <img className="w-16 h-16" src="./scripai-logo.png" />
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col ">
               <h3 className="font-bold">ScripAI</h3>
               <h4 className="text-[8px]">
                 100% FREE AI 100+ AI copy tools for marketers and creators. save
@@ -105,10 +105,10 @@ export default function Home() {
           <a
             href="http://careerdekho.ai/"
             target="_blank"
-            className="flex gap-2 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
+            className="flex gap-1.5 justify-around items-center max-w-sm border hover:shadow rounded-md shadow-sm p-2"
           >
             <img className="w-16 h-16" src="./careerdekho-logo.png" />
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col ">
               <h3 className="font-bold">careerdekho ai</h3>
               <h4 className="text-[8px]">
                 AI Career suggestion & career discovery for students and working
@@ -118,7 +118,7 @@ export default function Home() {
             <img className="w-10 h-10 p-0.5 hover:scale-105" src="./link.svg" />
           </a>
           <a href="https://www.linkedin.com/in/sagarjaid/"
-          target="_blank"  className="text-center text-gray-400 text-xs">
+            target="_blank" className="text-center text-gray-400 text-xs">
             show more
           </a>
         </div>
